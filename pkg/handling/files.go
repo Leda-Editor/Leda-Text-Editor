1eaf971b37fd41ad01543b7a05c94dd494ef251b
Eoghan Murch <122402422@umail.ucc.ie>
2025-02-25 19:44:53 +0000

    added Save function alongside SaveAs function to allow saving of current files

--- pkg/handling/files.go
+++ pkg/handling/files.go
@@ -6,6 +6,7 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
+	"fyne.io/fyne/v2/storage"
 )
 
 var CurrentFile fyne.URI
@@ -29,12 +30,33 @@ func OpenFile(window fyne.Window, editor *widget.Entry) {
 		}
 
 		editor.SetText(string(data))
-		CurrentFile = reader.URI()
+		CurrentFile = reader.URI() //stores current url
 	}, window)
 }
 
-// opens a file dialog and saves the editor's content to the selected file.
+// saves to the currently open file
 func SaveFile(window fyne.Window, editor *widget.Entry) {
+	if CurrentFile == nil {
+		SaveFileAs(window, editor) // No file is open, so use SaveFileAs instead
+		return
+	}
+	// open the current file for writing
+	writer, err := storage.Writer(CurrentFile)
+	if err != nil {
+		dialog.ShowError(err, window)
+		return
+	}
+	defer writer.Close()
+	// write the content
+	_, err = writer.Write([]byte(editor.Text))
+	if err != nil {
+		dialog.ShowError(err, window)
+		return
+	}
+}
+
+// opens a file dialog and saves the editor's content to the selected file.
+func SaveFileAs(window fyne.Window, editor *widget.Entry) {
 	dialog.ShowFileSave(func(writer fyne.URIWriteCloser, err error) {
 		if err != nil {
 			dialog.ShowError(err, window)
@@ -45,16 +67,17 @@ func SaveFile(window fyne.Window, editor *widget.Entry) {
 		}
 		defer writer.Close()
 
-		_, err = writer.Write([]byte(editor.Text)) //converts to bytes and writes to file
+		_, err = writer.Write([]byte(editor.Text)) // converts to bytes and writes to file
 		if err != nil {
 			dialog.ShowError(err, window)
 			return
 		}
+		CurrentFile = writer.URI() // change current file to new location
 	}, window)
 }
 
-// clears the editor's content.
+// clears the editor's content and resets current file
 func ClearEditor(editor *widget.Entry) {
 	editor.SetText("")
-	CurrentFile = nil
+	CurrentFile = nil 
 }
